4902b0399f9d17675d77770917b31778a9293673
davidmitesh <excelrock_mitesh@yahoo.com>
2018-05-06 10:04:00 +0545

    initial commit

---
+++ playgrrund/mongodb-connect.js
@@ -0,0 +1,43 @@
+// const MongoClient=require('mongodb').MongoClient;
+const {MongoClient}=require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/toDoApp',(err,db)=>{
+  if (err)
+  {
+    return console.log("cant connect to server");
+  }
+  console.log("connected to server");
+
+  // db.collection('toDoApp').insertOne({
+  //   text:"something to do",
+  //   completed:false
+  // },(err,result)=>{
+  //   if (err)
+  //   {
+  //     return console.log("cant write data",err);
+  //   }
+  //   console.log(JSON.stringify(result.ops));
+  // })
+  // db.collection('Users').insertOne({
+  //   name:'mitesh',
+  //   age:20,
+  //   location:'kathmandu'
+  // },(err,result)=>{
+  //   if(err){
+  //     return console.log("cant insert the document",err);
+  //   }
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // })
+  db.collection('Users').find({name:'mitesh'})
+  .toArray().then((res)=>{
+    console.log(JSON.stringify(res,undefined,2))
+  },(err)=>{
+    console.log('unable to fetch data');
+  });
+    db.collection('Users').find({name:'mitesh'})
+  .count().then((res)=>{
+    console.log(`number of records is :${res}`);
+  },(err)=>{
+    console.log("cant count records");
+  });
+  db.close();
+});
